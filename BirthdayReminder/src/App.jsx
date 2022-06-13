c6fb4929a3094975b3ae036e9baa8ace9f4f7e43
mishoto <mihail.dev.io@gmail.com>
2022-06-13 12:11:35 +0300

    BR-1.0.0-initial commit

---
+++ BirthdayReminder/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+
+const App = () => (
+  
+    <h2>BirthdayReminder</h2>
+    
+  
+);
+ReactDOM.render(<App />, document.getElementById("app"));
