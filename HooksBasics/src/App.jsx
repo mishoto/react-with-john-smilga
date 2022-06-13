f43789a3868f21a54c70c4de2279dc9a401e282c
mishoto <mihail.dev.io@gmail.com>
2022-06-13 10:18:17 +0300

    HB-1.0.0-create useStateWithObject

--- HooksBasics/src/App.jsx
+++ HooksBasics/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import UseStateHook from './components/useState/UseState';
 import UseStateWithArray from './components/useState/UseStateWithArray';
+import UseStateWithObject from './components/useState/UseStateWithObject';
 
 import './index.css';
 
@@ -9,6 +10,7 @@ const App = () => (
   <div className='container'>
     <UseStateHook />
     <UseStateWithArray />
+    <UseStateWithObject />
   </div>
 );
 ReactDOM.render(<App />, document.getElementById('app'));
