d6b2f969538c847db34216fa17e17bea92420621
mishoto <mihail.dev.io@gmail.com>
2022-06-16 18:11:50 +0300

    feature/LI-1.0.0-add implementation to component

--- LoremIpsum/src/components/LoremIpsum.jsx
+++ LoremIpsum/src/components/LoremIpsum.jsx
@@ -1,21 +1,43 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
+import data from './data';
 
 const LoremIpsum = () => {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
 
-  const handleSubmit = ()=> {
-
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > data.length) {
+      amount = data.length;
+    }
+    setText(data.slice(0, amount));
+  };
   return (
     <>
-    <form className='lorem-form' onSubmit={handleSubmit}>
-      <label htmlFor='amount'>paragraphs:</label>
-      
-    </form>
-    <article className='lorem-text'>
-
-    </article>
+      <form className='lorem-form' onSubmit={handleSubmit}>
+        <label htmlFor='amount'>paragraphs:</label>
+        <input
+          type='number'
+          name='amount'
+          id='amount'
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button className='btn' type='submit'>
+          generate
+        </button>
+      </form>
+      <article className='lorem-text'>
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </article>
     </>
-  )
-}
+  );
+};
 
-export default LoremIpsum
+export default LoremIpsum;
