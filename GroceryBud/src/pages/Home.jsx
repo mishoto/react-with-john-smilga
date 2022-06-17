b36e99fae6e21c4db7c73ccf7fe40d2419db4e84
mishoto <mihail.dev.io@gmail.com>
2022-06-17 11:05:19 +0300

    GB-1.0.0- create Home.jsx with components setup

---
+++ GroceryBud/src/pages/Home.jsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react';
+import List from '../components/List';
+import Alert from '../components/Alert';
+
+const Home = () => {
+  const [name, setName] = useState('');
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name) {
+      //display alert
+    } else if (name && isEditing) {
+      //deal with edit
+    } else {
+      //show alert
+      const newItem = {
+        id: new Date().getTime().toString(), // external lib UUID!
+        title: name,
+      };
+      setList([...list], newItem);
+      setName('');
+    }
+  };
+
+  return (
+    <section className='section-center'>
+      <form className='grocery-form' onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
+        <h3>grocery bud</h3>
+        <div className='form-control'>
+          <input
+            type='text'
+            className='grocery'
+            placeholder='item'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type='submit' className='submit-btn'>
+            {isEditing ? 'edit' : 'submit'}
+          </button>
+        </div>
+      </form>
+      <div className='grosery-container'>
+        <List items={list}/>
+        <button className='clear-btn'>clear items</button>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
