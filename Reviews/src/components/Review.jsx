f42909c29aef4e32507bac84d87d420efd731f4f
mishoto <mihail.dev.io@gmail.com>
2022-06-17 21:29:26 +0300

    update/RS-1.0.0-implement prev, next & random preview

--- Reviews/src/components/Review.jsx
+++ Reviews/src/components/Review.jsx
@@ -1,10 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react';
 import people from '../../data';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-  return (
-    <h3>Review</h3>
-  )
-}
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = people[index];
 
-export default Review
+  const checkIndex = (index) => {
+    if (index > people.length - 1) {
+      return 0;
+    }
+    if (index < 0) {
+      return people.length - 1;
+    }
+    return index;
+  };
+
+  const prevPerson = () => {
+    setIndex((index) => {
+      let newIndex = index - 1;
+      return checkIndex(newIndex);
+    });
+  };
+  const nextPerson = () => {
+    setIndex((index) => {
+      let newIndex = index + 1;
+      return checkIndex(newIndex);
+    });
+  };
+  const randomPerson = () => {
+    setIndex(() => {
+      let newIndex = Math.floor(Math.random() * people.length);
+      return newIndex;
+    });
+  };
+  return (
+    <article className='review'>
+      <div className='img-container'>
+        <img src={image} alt={name} className='person-img' />
+        <span className='quote-icon'>
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className='author'>{name}</h4>
+      <p className='job'>{job}</p>
+      <p className='info'>{text}</p>
+      <div className='button-container'>
+        <button className='prev-btn' onClick={prevPerson}>
+          <FaChevronLeft />
+        </button>
+        <button className='next-btn' onClick={nextPerson}>
+          <FaChevronRight />
+        </button>
+      </div>
+      <button className='random-btn' onClick={randomPerson}>
+        surprise me
+      </button>
+    </article>
+  );
+};
+
+export default Review;
