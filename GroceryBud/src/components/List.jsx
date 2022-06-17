5175d1af647e97780aec49cc215f04a74c7ad2d2
mishoto <mihail.dev.io@gmail.com>
2022-06-17 18:41:07 +0300

    update/GB-1.0.0-add edit & delete list items

--- GroceryBud/src/components/List.jsx
+++ GroceryBud/src/components/List.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { FaEdit, FaTrash } from 'react-icons/fa';
 
-const List = () => {
+const List = ({ items, editItem, removeItem }) => {
   return (
-    <h2>List Component</h2>
-  )
-}
+    <div className='grocery-list'>
+      {items.map((item) => {
+        const { id, title } = item;
+        return (
+          <article key={id} className='grocery-item'>
+            <p className='title'>{title}</p>
+            <div className='btn-container'>
+              <button
+                type='button'
+                className='edit-btn'
+                onClick={() => editItem(id)}>
+                <FaEdit />
+              </button>
+              <button
+                type='button'
+                className='delete-btn'
+                onClick={() => removeItem(id)}>
+                <FaTrash />
+              </button>
+            </div>
+          </article>
+        );
+      })}
+    </div>
+  );
+};
 
-export default List
+export default List;
