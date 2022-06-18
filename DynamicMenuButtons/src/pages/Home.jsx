a41f699aae1c9ce71d23e28ca0b282367dfcdb97
mishoto <mihail.dev.io@gmail.com>
2022-06-18 08:12:00 +0300

    feature/DMB-1.0.0- add implementation with Menu & Categories components

--- DynamicMenuButtons/src/pages/Home.jsx
+++ DynamicMenuButtons/src/pages/Home.jsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Categories from '../components/Categories';
 import Menu from '../components/Menu';
-import data from '../data';
+import items from '../data';
+
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+console.log(allCategories);
 
 const Home = () => {
-  return <section className='section menu'>
-    Home
-    </section>;
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => {
+      return item.category === category;
+    });
+    setMenuItems(newItems);
+  };
+  return (
+    <section className='section menu'>
+      <div className='title'>
+        <h3>Our menu</h3>
+        <div className='underline'></div>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu menuItems={menuItems} />
+      </div>
+    </section>
+  );
 };
 
 export default Home;
