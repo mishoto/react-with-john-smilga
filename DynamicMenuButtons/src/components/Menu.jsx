6c8aed7b3057efe59cf9f0b9ccb52873dd3b11f5
mishoto <mihail.dev.io@gmail.com>
2022-06-18 08:10:36 +0300

    update/DMB-1.0.0-add menu rendering

--- DynamicMenuButtons/src/components/Menu.jsx
+++ DynamicMenuButtons/src/components/Menu.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from 'react';
 
-const Menu = () => {
+const Menu = ({ menuItems }) => {
+  console.log(menuItems);
   return (
-    <article className='menu-item'>
-      
-      </article>
-  )
-}
+    <div className='section-center'>
+      {menuItems.map((item) => {
+        const { id, title, img, desc, price } = item;
+        return (
+          <article key={id} className='menu-item'>
+            <img src={img} alt={title} className='photo' />
+            <div className='item-info'>
+              <header>
+                <h4>{title}</h4>
+                <h4 className='price'>{price}</h4>
+              </header>
+              <p className='item-text'>{desc}</p>
+            </div>
+          </article>
+        );
+      })}
+    </div>
+  );
+};
 
-export default Menu
+export default Menu;
