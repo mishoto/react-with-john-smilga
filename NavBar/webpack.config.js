32316af0903536ca9e9ce8b14207343f9e134e15
mishoto <mihail.dev.io@gmail.com>
2022-06-19 10:40:44 +0300

    config/NB-1.0.0-update config for [mode, output, devtool, devServer] add rules for image files

--- NavBar/webpack.config.js
+++ NavBar/webpack.config.js
@@ -1,46 +1,59 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   output: {
-    publicPath: "http://localhost:3010/",
+    publicPath: 'http://localhost:3010/',
+
+    path: path.resolve(__dirname, './dist'),
+    assetModuleFilename: 'images/[hash][ext][query]',
   },
 
+  devtool: 'source-map',
+
   resolve: {
-    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+    extensions: ['.tsx', '.ts', '.jsx', '.js', '.json'],
   },
 
   devServer: {
     port: 3010,
-    historyApiFallback: true,
+    static: {
+      directory: path.join(__dirname, './dist'),
+    },
+    hot: true,
   },
 
   module: {
     rules: [
       {
+        test: /\.(png|jpeg|jpg|gif|svg)$/,
+        type: 'asset/resource',
+      },
+      {
         test: /\.m?js/,
-        type: "javascript/auto",
+        type: 'javascript/auto',
         resolve: {
           fullySpecified: false,
         },
       },
       {
         test: /\.(css|s[ac]ss)$/i,
-        use: ["style-loader", "css-loader", "postcss-loader"],
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
       },
       {
         test: /\.(ts|tsx|js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
         },
       },
     ],
   },
 
   plugins: [
-    
     new HtmlWebPackPlugin({
-      template: "./src/index.html",
+      template: './src/index.html',
     }),
   ],
 };
