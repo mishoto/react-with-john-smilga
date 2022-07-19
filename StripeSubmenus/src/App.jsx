4bf7adb3e9c762480c7b609585341ef03a5e4e67
mishoto <mihail.dev.io@gmail.com>
2022-07-19 09:54:24 +0300

    update/STP-1.0.0-add components to App

--- StripeSubmenus/src/App.jsx
+++ StripeSubmenus/src/App.jsx
@@ -1,12 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Sidebar from './components/Sidebar';
+import Submenu from './components/Submenu';
+import { AppProvider } from './context';
 
-import "./index.css";
+import './index.css';
 
 const App = () => (
   <>
-    <h3>Stripe Submenus</h3>
-    
+    <h3 style={{ textAlign: 'center', margin: '2rem' }}>Stripe Submenus</h3>
+    <AppProvider>
+      <Navbar />
+      <Sidebar />
+      <Hero />
+      <Submenu />
+    </AppProvider>
   </>
 );
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById('app'));
