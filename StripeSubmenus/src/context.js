3563756d16e92396596c5fda70437ade248b5274
mishoto <mihail.dev.io@gmail.com>
2022-07-21 10:25:11 +0300

    update/STP-1.0.0- add location & page attribute

--- StripeSubmenus/src/context.js
+++ StripeSubmenus/src/context.js
@@ -1,11 +1,13 @@
 import React, { useState, useContext } from 'react';
-import { sublinks } from './data';
+import sublinks from './data';
 
-const AppContext = React.createContext();
+export const AppContext = React.createContext();
 
 export const AppProvider = ({ children }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+  const [location, setLocation] = useState({});
+  const [page, setPage] = useState({ page: '', links: [] });
 
   const openSidebar = () => {
     setIsSidebarOpen(true);
@@ -13,7 +15,10 @@ export const AppProvider = ({ children }) => {
   const closeSidebar = () => {
     setIsSidebarOpen(false);
   };
-  const openSubmenu = () => {
+  const openSubmenu = (text, coordinates) => {
+    const page = sublinks.find((link) => link.page === text);
+    setPage(page);
+    setLocation(coordinates);
     setIsSubmenuOpen(true);
   };
   const closeSubmenu = () => {
@@ -22,12 +27,14 @@ export const AppProvider = ({ children }) => {
   return (
     <AppContext.Provider
       value={{
-        isSubmenuOpen,
         isSidebarOpen,
         openSidebar,
         closeSidebar,
+        isSubmenuOpen,
         openSubmenu,
         closeSubmenu,
+        location,
+        page,
       }}>
       {children}
     </AppContext.Provider>
