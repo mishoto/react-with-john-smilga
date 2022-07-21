0e962d60205b3038a1972ce607b546d749a86f7b
mishoto <mihail.dev.io@gmail.com>
2022-07-21 10:27:50 +0300

    update/STP-1.0.0- add displaySubmenu & handleSubmenu functions

--- StripeSubmenus/src/components/Navbar.jsx
+++ StripeSubmenus/src/components/Navbar.jsx
@@ -5,8 +5,22 @@ import { FaBars } from 'react-icons/fa';
 
 const Navbar = () => {
   const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
+  const displaySubmenu = (e) => {
+    const page = e.target.textContent;
+    const btnLocation = e.target.getBoundingClientRect();
+    const centerBtnLocation = (btnLocation.left + btnLocation.right) / 2;
+    const topSubmenu = btnLocation.bottom - 3;
+    openSubmenu(page, { centerBtnLocation, topSubmenu });
+  };
+
+  const handleSubmenu = (e) =>{
+    if(!e.target.classList.contains('link-btn')){
+      closeSubmenu();
+    }
+  }
+
   return (
-    <nav className='nav'>
+    <nav className='nav' onMouseOver = {handleSubmenu}>
       <div className='nav-center'>
         <div className='nav-header'>
           <img src={logo} alt='stripe' className='nav-logo' />
@@ -16,14 +30,20 @@ const Navbar = () => {
         </div>
         <ul className='nav-links'>
           <li>
-            <button className='link-btn'>products</button>
+            <button className='link-btn' onMouseOver={displaySubmenu}>
+              products
+            </button>
           </li>
           <li>
-            <button className='link-btn'>developers</button>
+            <button className='link-btn' onMouseOver={displaySubmenu}>
+              developers
+            </button>
           </li>
 
           <li>
-            <button className='link-btn'>company</button>
+            <button className='link-btn' onMouseOver={displaySubmenu}>
+              company
+            </button>
           </li>
         </ul>
         <button className='btn signin-btn'>Sign In</button>
