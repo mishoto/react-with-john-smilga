26167e4832162542dcd5933e60a9d17540adbe6f
mishoto <mihail.dev.io@gmail.com>
2022-07-27 10:13:41 +0300

    update/CT-1.0.0- implement clearCart

--- Cart/src/context.js
+++ Cart/src/context.js
@@ -1,24 +1,35 @@
 import React, { useState, useContext, useReducer, useEffect } from 'react';
 import cartItems from './data';
 import reducer from './reducer';
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
+
 const url = 'https://course-api.com/react-useReducer-cart-project';
 const AppContext = React.createContext();
 
+const initialState = {
+  loading: false,
+  cart: cartItems,
+  total: 0,
+  amount: 0,
+};
+
 const AppProvider = ({ children }) => {
-  const [cart, setCart] = useState(cartItems);
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const clearCart = () => {
+    dispatch({ type: 'CLEAR_CART' });
+  };
 
   return (
     <AppContext.Provider
       value={{
-        cart,
+        ...state,
+        clearCart,
       }}>
       {children}
     </AppContext.Provider>
   );
 };
-// make sure use
+
 export const useGlobalContext = () => {
   return useContext(AppContext);
 };
