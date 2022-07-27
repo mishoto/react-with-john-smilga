3486274678e0feaea401ca7fa3f2869aa894cf4c
mishoto <mihail.dev.io@gmail.com>
2022-07-27 10:38:24 +0300

    update/CT-1.0.0- implement removeItem

--- Cart/src/reducer.js
+++ Cart/src/reducer.js
@@ -1,6 +1,14 @@
+import CartItem from './components/CartItem';
+
 const reducer = (state, action) => {
   if (action.type === 'CLEAR_CART') {
     return { ...state, cart: [] };
+  }
+  if (action.type === 'REMOVE_ITEM') {
+    return {
+      ...state,
+      cart: state.cart.filter((cartItem) => cartItem.id !== action.payload),
+    };
   }
   return state;
 };
