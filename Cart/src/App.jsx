7fc75f20e57789d240197f31f0cee373a8d0225e
mishoto <mihail.dev.io@gmail.com>
2022-07-27 10:16:02 +0300

    update/CT-1.0.0-add loading... to App.jsx

--- Cart/src/App.jsx
+++ Cart/src/App.jsx
@@ -2,16 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CartContainer from './components/CartContainer';
 import Navbar from './components/Navbar';
-import { AppProvider } from './context';
+import { AppProvider, useGlobalContext } from './context';
 
 import './index.css';
 
-const App = () => (
-  <main>
-    <Navbar />
-    <CartContainer />
-  </main>
-);
+
+const App = () => {
+  const { loading } = useGlobalContext();
+  if (loading) {
+    return (
+      <div className='loading'>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+  return (
+    <main>
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
+};
 ReactDOM.render(
   <React.StrictMode>
     <AppProvider>
