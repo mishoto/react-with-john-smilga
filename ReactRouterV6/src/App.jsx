50c012e11d0841f1a0ffdc2f0d9546f20b865482
mishoto <mihail.dev.io@gmail.com>
2022-07-28 13:25:16 +0300

    update/RR-1.0.0- add ProtectedRoute wrapper

--- ReactRouterV6/src/App.jsx
+++ ReactRouterV6/src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
@@ -9,30 +9,34 @@ import SharedLayout from './shared/SharedLayout';
 import SingleProduct from './pages/SingleProduct';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
+import ProtectedRoute from './ProtectedRoute';
 
 import './index.css';
 
-
-
 const App = () => {
-
   const [user, setUser] = useState(null);
 
-  
-return(
-  <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<SharedLayout />}>
-        <Route index element={<Home />} />
-        <Route path='about' element={<About />} />
-        <Route path='products' element={<Products />} />
-        <Route path='products/:productId' element={<SingleProduct />} />
-        <Route path='login' element={<Login setUser={setUser} />} />
-        <Route path='dashboard' element={<Dashboard user={user} />} />
-      </Route>
-      <Route path='*' element={<Error />} />
-    </Routes>
-  </BrowserRouter>
-)
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<SharedLayout />}>
+          <Route index element={<Home />} />
+          <Route path='about' element={<About />} />
+          <Route path='products' element={<Products />} />
+          <Route path='products/:productId' element={<SingleProduct />} />
+          <Route path='login' element={<Login setUser={setUser} />} />
+          <Route
+            path='dashboard'
+            element={
+              <ProtectedRoute user={user}>
+                <Dashboard user={user} />
+              </ProtectedRoute>
+            }
+          />
+        </Route>
+        <Route path='*' element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 ReactDOM.render(<App />, document.getElementById('app'));
