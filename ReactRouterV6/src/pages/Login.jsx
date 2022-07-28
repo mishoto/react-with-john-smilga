7489f20cac3771704e805fa2f0b0ddffa7ad55d5
mishoto <mihail.dev.io@gmail.com>
2022-07-28 09:29:07 +0300

    RR-1.0.0- initial commit

---
+++ ReactRouterV6/src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <section className='section'>
+      <h3>Login</h3>
+    </section>
+  );
+}
+
+export default Login
---
+++ ReactRouterV6/src/pages/SingleProduct.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SingleProduct = () => {
+  return (
+    <section className='section'>
+      <h3>SingleProduct</h3>
+    </section>
+  );
+}
+
+export default SingleProduct
