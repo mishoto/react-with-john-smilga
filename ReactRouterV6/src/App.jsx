d66d51fd2d762f4fd3659d39fc2ba065af4d6986
mishoto <mihail.dev.io@gmail.com>
2022-07-28 12:13:25 +0300

    update/RR-1.0.0- implement Product & SingleProduct logic

--- ReactRouterV6/src/App.jsx
+++ ReactRouterV6/src/App.jsx
@@ -6,6 +6,7 @@ import About from './pages/About';
 import Products from './pages/Products';
 import Error from './pages/Error';
 import SharedLayout from './shared/SharedLayout';
+import SingleProduct from './pages/SingleProduct'
 
 import './index.css';
 
@@ -18,6 +19,7 @@ const App = () => (
         <Route index element={<Home/>}/>
         <Route path='about' element={<About />} />
         <Route path='products' element={<Products />} />
+        <Route path='products/:productId' element={<SingleProduct />} />
       </Route>
       <Route path='*' element={<Error/>}/>
     </Routes>
--- ReactRouterV6/src/pages/Products.jsx
+++ ReactRouterV6/src/pages/Products.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import products from '../data'
 
 const Products = () => {
   return (
     <section className='section'>
       <h3>Products</h3>
+      <div className="products">
+        {products.map((product) =>{
+          return(
+            <article key={product.id}>
+              <h5>{product.name}</h5>
+              <Link to={`/products/${product.id}`}>more info</Link>
+            </article>
+          )
+        })}
+      </div>
     </section>
   );
 }
