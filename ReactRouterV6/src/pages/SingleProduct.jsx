e2b80fccc8f3e7a6a7ba11a63682b4bc37ddfaf5
mishoto <mihail.dev.io@gmail.com>
2022-07-28 12:13:25 +0300

    update/RR-1.0.0- implement Product & SingleProduct logic

--- ReactRouterV6/src/pages/SingleProduct.jsx
+++ ReactRouterV6/src/pages/SingleProduct.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import {Link, useParams} from 'react-router-dom'
+import products from '../data'
 
 const SingleProduct = () => {
+  const {productId} = useParams();
+  const singleProduct = products.find((product)=> product.id===productId);
+  const {id, image, name} = singleProduct;
+  
   return (
-    <section className='section'>
-      <h3>SingleProduct</h3>
+    <section className='section product'>
+      <h3>{id}</h3>
+      <h4>{name}</h4>
+      <img src={image} alt={name}/>
+      <Link to='/products' className='btn'>back to products</Link>
     </section>
   );
 }
