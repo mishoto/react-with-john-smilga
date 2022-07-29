3880037a8457ea3395ca7fab8daeb04bb553d153
mishoto <mihail.dev.io@gmail.com>
2022-07-29 12:11:01 +0300

    update/CTA-1.0.0- implement Navbar & About & Error

--- CocktailsApi/src/components/Navbar.jsx
+++ CocktailsApi/src/components/Navbar.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../images/logo.svg';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className='navbar'>
+      <div className='nav-center'>
+        <Link to='/'>
+          <img src={logo} alt='logo' className='logo' />
+        </Link>
+        <ul className='nav-links'>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/about'>About</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
