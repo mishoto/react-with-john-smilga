98291b4d306cfea061dc618618777d8657d1e430
mishoto <mihail.dev.io@gmail.com>
2022-07-29 20:08:52 +0300

    update/CTA-1.0.0- add implementation logic

--- CocktailsApi/src/components/CocktailList.jsx
+++ CocktailsApi/src/components/CocktailList.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { useGlobalContext } from '../context';
+import Loading from './Loading';
+import Cocktail from './Cocktail';
 
 const CocktailList = () => {
-  return (
-    <div>CocktailList</div>
-  )
-}
+  const { loading, cocktails } = useGlobalContext();
+  if (loading) {
+    return <Loading />;
+  }
+  if (cocktails.length < 1) {
+    return <h2 className='section-title'>no cocktails matched your search</h2>;
+  }
 
-export default CocktailList
+  return (
+    <section className='section'>
+      <h2 className='section-title'>cocktails</h2>
+      <div className='cocktails-center'>
+        {cocktails.map((item) => {
+          return <Cocktail key={item.id} {...item} />;
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default CocktailList;
