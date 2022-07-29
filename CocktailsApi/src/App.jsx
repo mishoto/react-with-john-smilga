f3902e5209a7a080e15529d67d6829b44ef7c82d
mishoto <mihail.dev.io@gmail.com>
2022-07-29 12:12:01 +0300

    update/CTA-1.0.0- add Routing paths to App

--- CocktailsApi/src/App.jsx
+++ CocktailsApi/src/App.jsx
@@ -1,12 +1,30 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AppProvider } from './context';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import SingleCocktail from './pages/SingleCocktail'
+import Error from './pages/Error';
+import './index.css';
 
 const App = () => (
-  <div className="container">
-    <h3>CocktailsApi</h3>
-    
-  </div>
+  <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='about' element={<About/>} />
+      <Route path='/cocktail/:id' element={<SingleCocktail/>} />
+      <Route path='*' element={<Error/>} />
+    </Routes>
+  </BrowserRouter>
 );
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(
+  <React.StrictMode>
+    <AppProvider>
+      <App />
+    </AppProvider>
+  </React.StrictMode>,
+  document.getElementById('app'),
+);
